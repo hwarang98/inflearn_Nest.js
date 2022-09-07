e423487e491057a561b6205edce0198f361ec323
LeeHwaRang <snow2271@gmail.com>
2022-09-07 23:36:36 +0900

    nest.js pipe 학습

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,17 +1,34 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { PositiveIntPipe } from '../common/pipes/positivelnt.pipe';
+import { HttpExceptionFilter } from '../http-exception.filter';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCat() {
+    throw new HttpException('api is broken', 401);
     return 'all cat';
   }
 
   @Get(':id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
     return 'one cat';
   }
 
