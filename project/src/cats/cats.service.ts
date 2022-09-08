a8cf785a4f5553907b5ce6a3774b9bfa9e090e31
LeeHwaRang <snow2271@gmail.com>
2022-09-08 14:01:29 +0900

    서비스 로직에 회원가입 api 추가

--- project/src/cats/cats.service.ts
+++ project/src/cats/cats.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
 
 @Injectable()
 export class CatsService {
-  hiCatService() {
-    return 'hello cat service!';
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async signup(body: CatRequestDto) {
+    const { email, name, password } = body;
+    const isCatExist = await this.catModel.exists({ email });
+
+    if (isCatExist) {
+      throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.'); // UnauthorizedException = 403 err
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 15); // 비밀번호 암호화
+
+    const cat = await this.catModel.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+    return cat.readOnlyData;
   }
 }
