a283f5c9272a1bd383496580210ce238285d01ae
LeeHwaRang <snow2271@gmail.com>
2022-09-08 14:01:06 +0900

    virtual 추가

--- project/src/cats/cats.schema.ts
+++ project/src/cats/cats.schema.ts
@@ -33,6 +33,17 @@ export class Cat extends Document {
   @Prop()
   @IsString()
   imgUrl: string;
+
+  readonly readOnlyData: { id: string; email: string; name: string };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
+
+// 사용자에게 보여줄 데이터
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+  };
+});
