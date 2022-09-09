6ade2f3fd042d855ed828be7dc49637a87eb8d34
LeeHwaRang <snow2271@gmail.com>
2022-09-09 22:25:26 +0900

    주석 추가

--- project/src/main.ts
+++ project/src/main.ts
@@ -5,7 +5,7 @@ import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe()); // 등록해야지 클래스 벨리데이션이 작동함
   app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT;
   await app.listen(PORT);
