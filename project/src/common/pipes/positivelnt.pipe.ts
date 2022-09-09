64899367b9f574f0f7c140d828b990550f6cbc84
LeeHwaRang <snow2271@gmail.com>
2022-09-09 22:25:10 +0900

    코드정리

--- project/src/common/pipes/positivelnt.pipe.ts
+++ project/src/common/pipes/positivelnt.pipe.ts
@@ -3,7 +3,6 @@ import { HttpException, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class PositiveIntPipe implements PipeTransform {
   transform(value: number) {
-    // console.log('value: ', value);
     if (value < 0) {
       throw new HttpException('value > 0', 400);
     }
