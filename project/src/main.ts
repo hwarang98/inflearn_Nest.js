fa187191a92f3686b74d5b8c77bb8a531bd32a5f
LeeHwaRang <snow2271@gmail.com>
2022-09-10 13:05:32 +0900

    스웨거 추가

--- project/src/main.ts
+++ project/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
@@ -7,6 +8,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 등록해야지 클래스 벨리데이션이 작동함
   app.useGlobalFilters(new HttpExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('cat')
+    .setVersion('1.0.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const PORT = process.env.PORT;
   await app.listen(PORT);
 }
