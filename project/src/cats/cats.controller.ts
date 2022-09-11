42e46bc1618f14fe7fdf524b6a2575aa2a551736
LeeHwaRang <snow2271@gmail.com>
2022-09-11 21:45:20 +0900

    api 설명 추가

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -17,6 +17,7 @@ import { HttpExceptionFilter } from '../common/exceptions/http-exception.filter'
 import { CatsService } from './cats.service';
 import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
 import { CatRequestDto } from './dto/cats.request.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('cats')
 @UseInterceptors(SuccessInterceptor)
@@ -25,27 +26,34 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @ApiOperation({ summary: '현재 고양이 가져오기' })
   getCurrentCat() {
     return 'current cat';
   }
 
   @Post()
+  @ApiOperation({ summary: '회원가입' })
+  @ApiResponse({ status: 500, description: '서버에러' })
+  @ApiResponse({ status: 200, description: '성공' })
   async signUp(@Body() body: CatRequestDto) {
     console.log('body: ', body);
     return await this.catsService.signup(body);
   }
 
   @Post('login')
+  @ApiOperation({ summary: '로그인' })
   logIn() {
     return 'signup';
   }
 
   @Post('logout')
+  @ApiOperation({ summary: '로그아웃' })
   logOut() {
     return 'logout';
   }
 
   @Post('upload/cats')
+  @ApiOperation({ summary: '고양이 사진 업로드' })
   uploadCatImg() {
     return 'uploadImg';
   }
