d3d280830d60f107edd3de947fc92d4faadac139
LeeHwaRang <snow2271@gmail.com>
2022-09-11 21:44:56 +0900

    swagger apiproperty 적용

--- project/src/cats/dto/cats.request.dto.ts
+++ project/src/cats/dto/cats.request.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CatRequestDto {
+  @ApiProperty({
+    example: 'test@gmail.com',
+    description: 'email',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    example: '1234',
+    description: 'password',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({
+    example: '김철수',
+    description: 'name',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
