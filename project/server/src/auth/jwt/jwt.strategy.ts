db85123a2db865adb24a3f79dc0176c73000b988
LeeHwaRang <snow2271@gmail.com>
2022-09-16 14:39:05 +0900

    페이로드 유효성 검사

--- project/server/src/auth/jwt/jwt.strategy.ts
+++ project/server/src/auth/jwt/jwt.strategy.ts
@@ -1,16 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { CatsRepository } from '../../cats/cats.repository';
+import { Payload } from './jwt.payload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   // 인증할때 사용
-  constructor() {
+  constructor(private readonly catsRepository: CatsRepository) {
+    // jwt에 대한 설정
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 헤더에 토큰으로부터 추출
       secretOrKey: 'secret', // 환경변수로 저장
       ignoreExpiration: false, // 만료기간
     });
   }
-  // async validate(payload) {}
+
+  // 디코딩된 페이로드가 적합한지 판별
+  async validate(payload: Payload) {
+    const cat = await this.catsRepository.findCatByIdWithoutPassword(
+      payload.sub,
+    );
+    if (cat) {
+      return cat;
+    } else {
+      throw new UnauthorizedException('접근오류');
+    }
+  }
 }
