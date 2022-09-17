699d23e3ed7809b3523ff736fd1caac7ab7eb82f
LeeHwaRang <snow2271@gmail.com>
2022-09-17 20:10:23 +0900

    jwt 시크릿키 환경변수 설정

--- project/server/src/auth/auth.module.ts
+++ project/server/src/auth/auth.module.ts
@@ -4,14 +4,16 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { CatsModule } from '../cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
 
     // jwt 모듈을 만드는 곳
     JwtModule.register({
-      secret: 'secret',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1y' },
     }),
     forwardRef(() => CatsModule),
