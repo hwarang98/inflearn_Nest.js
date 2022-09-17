8a2cb8d5d747c01317749f855714af959c713e47
LeeHwaRang <snow2271@gmail.com>
2022-09-17 20:13:09 +0900

    swagger 보안설정

--- project/server/src/main.ts
+++ project/server/src/main.ts
@@ -3,11 +3,21 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 등록해야지 클래스 벨리데이션이 작동함
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(
+    ['/api', 'api-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('C.I.C')
     .setDescription('cat')
