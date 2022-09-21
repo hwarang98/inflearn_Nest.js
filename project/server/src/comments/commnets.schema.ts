be949a308c14d498a03490a32ebfc6fa36fa9b7a
LeeHwaRang <snow2271@gmail.com>
2022-09-21 22:57:17 +0900

    댓글기능 스키마

---
+++ project/server/src/comments/commnets.schema.ts
@@ -0,0 +1,71 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Document, Types } from 'mongoose';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Comments extends Document {
+  @ApiProperty({
+    description: 'email',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true, // 반드시 필요 유무
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  author: Types.ObjectId;
+
+  @ApiProperty({
+    description: 'name',
+    required: true,
+  })
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'password',
+    required: true,
+  })
+  @Prop({
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @Prop({
+    default:
+      ' https://raw.githubusercontent.com/amamov/teaching-nestjs-a-to-z/main/images/1.jpeg',
+  })
+  @IsString()
+  imgUrl: string;
+
+  readonly readOnlyData: {
+    id: string;
+    email: string;
+    name: string;
+    imgUrl: string;
+  };
+}
+
+export const CatSchema = SchemaFactory.createForClass(Cat);
+
+// 사용자에게 보여줄 데이터
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+    imgUrl: this.imgUrl,
+  };
+});
