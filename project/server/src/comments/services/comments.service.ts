d5a8079311b37ff282cc7cf089a74889875b6794
LeeHwaRang <snow2271@gmail.com>
2022-09-23 15:46:54 +0900

    댓글기능, 댓글좋아요, 댓글 전체조회 구현

--- project/server/src/comments/services/comments.service.ts
+++ project/server/src/comments/services/comments.service.ts
@@ -1,17 +1,50 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CatsRepository } from '../../cats/cats.repository';
+import { Comments } from '../commnets.schema';
 import { CommnetsCreateDto } from '../dto/comments.create.dto';
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    @InjectModel(Comments.name) private readonly commentsModel: Model<Comments>,
+    private readonly catsRepository: CatsRepository,
+  ) {}
+
   async getAllComments() {
-    return 'all comments';
+    try {
+      const comments = await this.commentsModel.find();
+      return comments;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
-  async createComment(id: string, commnets: CommnetsCreateDto) {
-    Logger.log(commnets);
-    return `id: ${id}, commnets: [${commnets.author}, ${commnets.contents}]`;
+
+  async createComment(id: string, commnetData: CommnetsCreateDto) {
+    try {
+      const targetCat = await this.catsRepository.findCatByIdWithoutPassword(
+        id,
+      );
+      const { contents, author } = commnetData;
+      const validatedAuthor =
+        await this.catsRepository.findCatByIdWithoutPassword(author);
+      const newCommnet = new this.commentsModel({
+        author: validatedAuthor._id,
+        contents,
+        info: targetCat._id,
+      });
+      return await newCommnet.save();
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
   async plustLike(id: string) {
-    return '좋아요수 올리기';
+    try {
+      const comment = await this.commentsModel.findById(id);
+      comment.likeCount += 1;
+      return await comment.save();
+    } catch (error) {}
   }
 }
