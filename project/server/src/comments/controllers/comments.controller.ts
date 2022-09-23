5860f0544b93f6f34094138ddcad4e59381ba38c
LeeHwaRang <snow2271@gmail.com>
2022-09-23 15:46:31 +0900

    post -> patch 변경

--- project/server/src/comments/controllers/comments.controller.ts
+++ project/server/src/comments/controllers/comments.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { CommnetsCreateDto } from '../dto/comments.create.dto';
 import { CommentsService } from '../services/comments.service';
@@ -23,7 +23,7 @@ export class CommentsController {
   }
 
   @ApiOperation({ summary: '좋아요 수 올리기' })
-  @Post(':id')
+  @Patch(':id')
   async plusLike(@Param('id') id: string) {
     return this.commnetService.plustLike(id);
   }
