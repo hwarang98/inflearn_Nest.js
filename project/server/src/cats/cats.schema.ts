3252d139ecbc76cbe7d9b96a1b240401818d371b
LeeHwaRang <snow2271@gmail.com>
2022-09-23 16:20:52 +0900

    스키마 수정

--- project/server/src/cats/cats.schema.ts
+++ project/server/src/cats/cats.schema.ts
@@ -2,6 +2,7 @@ import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { Document } from 'mongoose';
+import { Comments } from '../comments/commnets.schema';
 
 const options: SchemaOptions = {
   timestamps: true,
@@ -58,17 +59,32 @@ export class Cat extends Document {
     email: string;
     name: string;
     imgUrl: string;
+    comments: Comments[];
   };
+  readonly comments: Comments[];
 }
 
-export const CatSchema = SchemaFactory.createForClass(Cat);
+export const _CatSchema = SchemaFactory.createForClass(Cat);
 
 // 사용자에게 보여줄 데이터
-CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+_CatSchema.virtual('readOnlyData').get(function (this: Cat) {
   return {
     id: this.id,
     email: this.email,
     name: this.name,
     imgUrl: this.imgUrl,
+    comments: this.comments,
   };
 });
+
+_CatSchema.virtual('comments', {
+  ref: 'comments', // 스키마이름 즉, 컬렉션 이름
+  localField: '_id',
+  foreignField: 'info', // comments 스키마의 info를 가져옴
+});
+
+// 포풀레이트 설정 (다른 도큐먼트랑 이어주는 메서드)
+_CatSchema.set('toObject', { virtuals: true });
+_CatSchema.set('toJSON', { virtuals: true });
+
+export const CatSchema = _CatSchema;
