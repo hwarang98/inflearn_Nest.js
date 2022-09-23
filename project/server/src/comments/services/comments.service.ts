f327ce5bfca8d84874289cf69122e901ed9d11d9
LeeHwaRang <snow2271@gmail.com>
2022-09-23 13:55:17 +0900

    댓글기능 서비스 로직 작성

--- project/server/src/comments/services/comments.service.ts
+++ project/server/src/comments/services/comments.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { CommnetsCreateDto } from '../dto/comments.create.dto';
 
 @Injectable()
 export class CommentsService {
   async getAllComments() {
     return 'all comments';
   }
-  async createComment(id: string) {
-    return `id: ${id}`;
+  async createComment(id: string, commnets: CommnetsCreateDto) {
+    Logger.log(commnets);
+    return `id: ${id}, commnets: [${commnets.author}, ${commnets.contents}]`;
+  }
+
+  async plustLike(id: string) {
+    return '좋아요수 올리기';
   }
 }
